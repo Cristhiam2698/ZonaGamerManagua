24d08d02e4ad557217889a818546e3719ee2e05e
Nahum Andres <71681723+Andres179012@users.noreply.github.com>
2021-10-23 22:37:34 -0600

    Implementacion del Template

--- ZonaGamerN/obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
+++ ZonaGamerN/obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
@@ -13,70 +13,70 @@ namespace ZonaGamerN
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 1 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 2 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 3 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 4 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 5 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 6 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 7 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 8 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 9 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using ZonaGamerN;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 10 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using ZonaGamerN.Shared;
 
 #line default
--- ZonaGamerN/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ ZonaGamerN/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,70 +13,70 @@ namespace ZonaGamerN.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 1 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 2 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 3 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 4 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 5 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 6 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 7 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 8 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 9 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using ZonaGamerN;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\_Imports.razor"
+#line 10 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\_Imports.razor"
 using ZonaGamerN.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using ZonaGamerN.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "C:\Users\Cristhiam Padilla\source\repos\ZonaGamerN\ZonaGamerN\ZonaGamerN\Shared\SurveyPrompt.razor"
+#line 12 "C:\Users\Andres\Desktop\ZonaGamerManagua\ZonaGamerN\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
